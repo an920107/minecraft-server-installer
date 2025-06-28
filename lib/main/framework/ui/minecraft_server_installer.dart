89c3835d313dcbccc9e636b1f587bb56aba2fdf4
SquidSpirit <squid@squidspirit.com>
2025-06-29 02:13:25 +0800

    MCSI-1 feat: remove template

---
+++ lib/main/framework/ui/minecraft_server_installer.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MinecraftServerInstaller extends StatelessWidget {
+  const MinecraftServerInstaller({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:minecraft_server_installer/main/framework/ui/minecraft_server_installer.dart';
+
+void main() {
+  runApp(const MinecraftServerInstaller());
+}
