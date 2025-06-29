b1aa09bf5791c442dbee453298ce4889d0030dc1
SquidSpirit <squid@squidspirit.com>
2025-06-29 18:15:55 +0800

    MCSI-1 feat: adjust window size

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:minecraft_server_installer/main/framework/ui/minecraft_server_installer.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  final windowOptions = const WindowOptions(
+    size: Size(400, 600),
+    minimumSize: Size(400, 600),
+    center: true,
+    backgroundColor: Colors.transparent,
+    skipTaskbar: false,
+    titleBarStyle: TitleBarStyle.hidden,
+  );
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
   runApp(const MinecraftServerInstaller());
 }
