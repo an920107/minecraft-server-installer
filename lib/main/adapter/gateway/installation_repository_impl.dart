94556a49d7baed7ae3aa216542db6f6aa50a96ce
SquidSpirit <squid@squidspirit.com>
2025-07-12 18:31:36 +0800

    MCSI-8 refactor: rename variables for clarity in InstallationRepositoryImpl

--- lib/main/adapter/gateway/installation_repository_impl.dart
+++ lib/main/adapter/gateway/installation_repository_impl.dart
@@ -3,22 +3,22 @@ import 'package:minecraft_server_installer/main/adapter/gateway/installation_fil
 import 'package:minecraft_server_installer/main/application/repository/installation_repository.dart';
 
 class InstallationRepositoryImpl implements InstallationRepository {
-  final InstallationApiService _apiService;
-  final InstallationFileStorage _fileStorage;
+  final InstallationApiService _installationApiService;
+  final InstallationFileStorage _installationFileStorage;
 
-  InstallationRepositoryImpl(this._apiService, this._fileStorage);
+  InstallationRepositoryImpl(this._installationApiService, this._installationFileStorage);
 
   @override
   Future<void> downloadFile(Uri url, String path, {DownloadProgressCallback? onProgressChanged}) async {
-    final fileBytes = await _apiService.fetchRemoteFile(url, onProgressChanged: onProgressChanged);
-    await _fileStorage.saveFile(fileBytes, path);
+    final fileBytes = await _installationApiService.fetchRemoteFile(url, onProgressChanged: onProgressChanged);
+    await _installationFileStorage.saveFile(fileBytes, path);
   }
 
   @override
-  Future<void> writeFile(String path, String content) => _fileStorage.writeFile(path, content);
+  Future<void> writeFile(String path, String content) => _installationFileStorage.writeFile(path, content);
 
   @override
   Future<void> grantFileExecutePermission(String path) {
-    return _fileStorage.grantFileExecutePermission(path);
+    return _installationFileStorage.grantFileExecutePermission(path);
   }
 }
